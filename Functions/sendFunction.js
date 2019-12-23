32c78349ff9dbd360b513d59435e3cc71b04ca53
himeenthusiast <isabellapiantoni@hotmail.com>
2019-12-23 17:15:09 -0500

    fixed send function

--- Functions/sendFunction.js
+++ Functions/sendFunction.js
@@ -10,7 +10,7 @@ function sendMessage(fs, bot, schedule) {
 
             //The time seemed to not want to change in the program without restarting? I'm not sure how to do that so I removed the functionality.
             //It will send messages at 8am, this is in 24 hour time
-            let send = schedule.scheduleJob({hour: 17, minute: 0}, function(){
+            schedule.scheduleJob('30 17 * * *', function(){ //SCHEDULE START
                 let fullDate = new Date();
 
                 //Formatting the date as it's recorded in the JSON file, to check date
@@ -61,7 +61,7 @@ function sendMessage(fs, bot, schedule) {
                         });
                     }
                 }
-            });
+            }); //SCHEDULE END
         }
     });
 };
